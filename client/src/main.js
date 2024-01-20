3557c654a8f183d421d132bde86665884abf2219
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-20 15:18:43 -0300

    ✨ Using router and hygraphClient in main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router/index.js'
 import { trackPageview, trackEvent } from "./analytics-api.js";
+import { GraphQLClient } from 'graphql-request';
 
-createApp(App).mount('#app')
+const hygraphClient = new GraphQLClient(
+  'https://api-sa-east-1.hygraph.com/v2/clrm4asx40h9y01w3qaztpy5a/master'
+);
+
+const app = createApp(App)
+app.mixin({
+  beforeCreate() {
+    this.$hygraph = hygraphClient;
+  },
+});
+app.use(router)
+app.mount('#app')
