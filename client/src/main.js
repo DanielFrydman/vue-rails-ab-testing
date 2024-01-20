f7b9d7e5aba82d1c1643d4cb6ad81eec5077c9ee
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-20 17:15:43 -0300

    💄 Improving readability

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router/index.js'
 import { trackPageview, trackEvent } from "./analytics-api.js";
 import { GraphQLClient } from 'graphql-request';
+import { createApp } from 'vue';
+import router from './router/index.js';
+import App from './App.vue';
+import './style.css';
 
 const hygraphClient = new GraphQLClient(
   'https://api-sa-east-1.hygraph.com/v2/clrm4asx40h9y01w3qaztpy5a/master'
