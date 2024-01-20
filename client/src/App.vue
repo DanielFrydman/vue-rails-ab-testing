86703a7345c0c9c80d82c11ad2acb3f8f0405da3
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-20 12:51:36 -0300

    🎉 Adding stuf from the sandbox to start project

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,9 +2,31 @@
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="text-center">
+    <p class="font-bold text-4xl my-6">Check out the Blinkist app</p>
+  
+    <img
+      width="300"
+      src="./assets/hero_image.jpg"
+      alt="Check out the Blinkist app"
+      class="mx-auto"
+    />
+  
+    <div>
+      <!-- Control variation -->
+      Meet the app that revolutionized reading.
+    </div>
+  
+    <div>
+      <!-- Test variation -->
+      Meet the app that has 18 million users.
+    </div>
+  
+    <div>
+      Thanks a lot for reading the article! <a href="/signup">SIGN UP</a> for
+      Blinkist.
+    </div>
+  </div>
 </template>
 
 <style scoped>
