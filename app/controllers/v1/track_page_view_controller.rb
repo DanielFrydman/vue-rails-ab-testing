3d65658465d30c93e2f92812303ab4ab717033e6
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-21 17:28:12 -0300

    ✨ Creating controller to track page views

---
+++ app/controllers/v1/track_page_view_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module V1
+  class TrackPageViewController < ApplicationController
+    def create
+      PageView.create!(user_id:, url:, displayed_text_variation:)
+
+      render json: { message: 'Page view tracked.' }, status: :created
+    rescue StandardError => e
+      render json: { message: "An error occured: #{e}." }, status: :internal_server_error
+    end
+
+    private
+
+    def user_id
+      params.require(:user_id)
+    end
+
+    def url
+      params.require(:url)
+    end
+
+    def displayed_text_variation
+      params.require(:displayed_text_variation)
+    end
+  end
+end
