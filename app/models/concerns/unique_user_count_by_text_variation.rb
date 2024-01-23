7d1f4c0eb48ba85ee4f9aaf13c008e449bd596fa
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-23 19:54:09 -0300

    🎨 Renaming module

--- app/models/concerns/unique_user_count_by_text_variation.rb
+++ app/models/concerns/unique_user_count_by_text_variation.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-module UniqUserCountByTextVariation
+module UniqueUserCountByTextVariation
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def uniq_user_count_by_text_variation(text_variations, collection)
+    def unique_user_count_by_text_variation(text_variations, collection)
       hash = {}
       text_variations.each do |text_variation|
         new_collection = collection.select { |element| element.displayed_text_variation == text_variation }
