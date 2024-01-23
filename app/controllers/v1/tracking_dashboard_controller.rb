9f4d43f766d32aaf8ab95f8ad17ef04753b02b1a
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-23 19:54:28 -0300

    ♻️ Refactoring controller to call service

--- app/controllers/v1/tracking_dashboard_controller.rb
+++ app/controllers/v1/tracking_dashboard_controller.rb
@@ -3,14 +3,11 @@
 module V1
   class TrackingDashboardController < ApplicationController
     def index
-      page_views_chart = PageViewsBarChartPresenter.new(total_uniq_page_views).chart_data
-      events_chart = EventsBarChartPresenter.new(hash: total_uniq_events, event_name:).chart_data
-      ctr_chart = ClickthroughRateBarChartPresenter.new(
-        total_uniq_page_views:,
-        total_uniq_events:
-      ).chart_data
+      charts = TrackingDashboardChartsGeneratorService.new(
+        start_date:, end_date:, event_name:
+      ).generate
 
-      render json: { page_views_chart:, events_chart:, ctr_chart: }, status: :ok
+      render json: { charts: }, status: :ok
     rescue StandardError => e
       render json: { message: "An error occurred: #{e}." }, status: :internal_server_error
     end
@@ -22,18 +19,6 @@ module V1
     end
 
     private
-
-    def total_uniq_page_views
-      @total_uniq_page_views ||= PageView.total_uniq_page_views_in_period_by_displayed_text_variation(
-        start_date:, end_date:
-      )
-    end
-
-    def total_uniq_events
-      @total_uniq_events ||= Event.total_uniq_events_in_period_by_displayed_text_variation_and_action(
-        event_name:, start_date:, end_date:
-      )
-    end
 
     def start_date
       params.require(:start_date)
