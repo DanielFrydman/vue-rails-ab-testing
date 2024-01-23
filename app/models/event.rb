cab6a8075a25eaf5dd9867c31756383f1a0fe29e
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-23 19:54:57 -0300

    ♻️ Refactoring models to return unique and total values at the same time to avoid n+1

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
-  include UniqUserCountByTextVariation
+  include UniqueUserCountByTextVariation
 
   scope :in_period, proc { |start_date, end_date|
-    where(created_at: start_date..end_date)
+    where(created_at: Time.zone.parse(start_date).beginning_of_day..Time.zone.parse(end_date).end_of_day)
   }
   scope :by_event_name, ->(event_name) { where(event_name:) }
 
-  def self.total_uniq_events_in_period_by_displayed_text_variation_and_action(event_name:, start_date:, end_date:)
+  def self.in_period_by_displayed_text_variation_and_action(event_name:, start_date:, end_date:)
     events_in_period_by_event_name = in_period(start_date, end_date).by_event_name(event_name)
     displayed_text_variations = events_in_period_by_event_name.pluck(:displayed_text_variation).uniq
-    uniq_user_count_by_text_variation(displayed_text_variations, events_in_period_by_event_name)
+
+    {
+      unique: unique_user_count_by_text_variation(displayed_text_variations, events_in_period_by_event_name),
+      total: events_in_period_by_event_name.group(:displayed_text_variation).count,
+      url: events_in_period_by_event_name.last&.url
+    }
   end
 end
--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
 class PageView < ApplicationRecord
-  include UniqUserCountByTextVariation
+  include UniqueUserCountByTextVariation
 
   scope :in_period, proc { |start_date, end_date|
-    where(created_at: start_date..end_date)
+    where(created_at: Time.zone.parse(start_date).beginning_of_day..Time.zone.parse(end_date).end_of_day)
   }
+  scope :by_url, ->(url) { where(url:) }
 
-  def self.total_uniq_page_views_in_period_by_displayed_text_variation(start_date:, end_date:)
-    page_views_in_period = in_period(start_date, end_date)
+  def self.in_period_by_displayed_text_variation_and_url(start_date:, end_date:, url:)
+    page_views_in_period = in_period(start_date, end_date).by_url(url)
     displayed_text_variations = page_views_in_period.pluck(:displayed_text_variation).uniq
-    uniq_user_count_by_text_variation(displayed_text_variations, page_views_in_period)
+
+    {
+      unique: unique_user_count_by_text_variation(displayed_text_variations, page_views_in_period),
+      total: page_views_in_period.group(:displayed_text_variation).count,
+      url: page_views_in_period.last&.url
+    }
   end
 end
