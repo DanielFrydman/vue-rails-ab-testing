b74daa5cde21c1bdb7327346949780ba7673e298
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-23 08:05:50 -0300

    ✅ Creating test cases for event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,5 +3,42 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#in_period' do
+    let!(:event) { create(:event) }
+
+    it 'return events in period' do
+      elements = Event.in_period(Time.zone.yesterday, Time.zone.tomorrow)
+      expect(elements.count).to(eq(1))
+      expect(elements.last).to(eq(event))
+    end
+  end
+
+  describe '#by_event_name' do
+    let!(:event) { create(:event) }
+
+    it 'return events by event name' do
+      elements = Event.by_event_name('sign_up')
+      expect(elements.count).to(eq(1))
+      expect(elements.last).to(eq(event))
+    end
+  end
+
+  describe '#total_uniq_events_in_period_by_displayed_text_variation_and_action' do
+    let!(:event) do
+      create(:event, displayed_text_variation: 'Random text.')
+    end
+
+    it 'return total uniq events in period by text variation and action' do
+      hash = Event.total_uniq_events_in_period_by_displayed_text_variation_and_action(
+        event_name: 'sign_up',
+        start_date: Time.zone.yesterday,
+        end_date: Time.zone.tomorrow
+      )
+      expect(hash).to(
+        eq(
+          { 'Random text.' => 1 }
+        )
+      )
+    end
+  end
 end
