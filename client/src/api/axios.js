10b254ed2b1305234c4d019fe78490e0d72e9c7b
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-23 17:36:57 -0300

    🚑 Adding URL for development and production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 function ApiClient() {
+  const baseURL = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/v1'
+    : 'https://vue-rails-ab-testing.onrender.com/v1';
+
   const api = axios.create({
-    baseURL: 'http://localhost:3000/v1'
+    baseURL: baseURL
   });
 
   return api;
