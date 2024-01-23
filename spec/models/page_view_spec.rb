f02ce6a3cdc7c5630a4426d95a5fec2771285236
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-23 08:05:56 -0300

    ✅ Creating test cases for page view model

--- spec/models/page_view_spec.rb
+++ spec/models/page_view_spec.rb
@@ -3,5 +3,31 @@
 require 'rails_helper'
 
 RSpec.describe PageView, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#in_period' do
+    let!(:page_view) { create(:page_view) }
+
+    it 'return page views in period' do
+      elements = PageView.in_period(Time.zone.yesterday, Time.zone.tomorrow)
+      expect(elements.count).to(eq(1))
+      expect(elements.last).to(eq(page_view))
+    end
+  end
+
+  describe '#total_uniq_events_in_period_by_displayed_text_variation_and_action' do
+    let!(:page_view) do
+      create(:page_view, displayed_text_variation: 'Random text.')
+    end
+
+    it 'return total uniq events in period by text variation and action' do
+      hash = PageView.total_uniq_page_views_in_period_by_displayed_text_variation(
+        start_date: Time.zone.yesterday,
+        end_date: Time.zone.tomorrow
+      )
+      expect(hash).to(
+        eq(
+          { 'Random text.' => 1 }
+        )
+      )
+    end
+  end
 end
