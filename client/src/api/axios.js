dfa91b237ece7705f22998df61b0a0f0847c0617
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-23 21:58:30 -0300

    🚀 Updating production URL because I made a mistake

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 function ApiClient() {
   const baseURL = process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000/v1'
-    : 'https://vue-rails-ab-testing.onrender.com/v1';
+    : 'https://vue-rails-ab-testing-3mmd.onrender.com/';
 
   const api = axios.create({
     baseURL: baseURL
